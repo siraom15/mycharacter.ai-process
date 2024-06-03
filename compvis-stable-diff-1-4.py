54f5d769393bd9cdbd91067bfdd6dbf56be644cd
siraom15 <siriwatjaiyungyuen@gmail.com>
2024-06-03 16:26:54 +0700

    fix: 🐛 uncomment

--- compvis-stable-diff-1-4.py
+++ compvis-stable-diff-1-4.py
@@ -7,20 +7,20 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 
 print("Device:", device)
 
-# # Float 16 is used for faster inference (but cannot use with cpu)
-# # Use torch.float32 if you want to use float32
-# pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+# Float 16 is used for faster inference (but cannot use with cpu)
+# Use torch.float32 if you want to use float32
+pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float32)
 
-# pipe.to(device)
+pipe.to(device)
 
-# # Define the prompt
-# prompt = "a cartoon of a happy cat playing in a garden"
+# Define the prompt
+prompt = "a cartoon of a happy cat playing in a garden"
 
-# # Generate the image
-# image = pipe(prompt).images[0]
+# Generate the image
+image = pipe(prompt).images[0]
 
-# # Save the image
-# image_path = "generated_cartoon.png"
-# image.save(image_path)
+# Save the image
+image_path = "generated_cartoon.png"
+image.save(image_path)
 
-# print(f"Image saved at {image_path}")
+print(f"Image saved at {image_path}")
